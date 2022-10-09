da1d99dd2238b64558d7c5dcd7ce73c9d7a5e01a
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2022-10-09 18:45:51 +0530

    ft: setup vitepress for v1 docs

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,14 @@
+export default {
+    title: 'Unbound Internal Docs',
+    description: 'Unbound v1 Internal Documentation',
+    themeConfig: {
+        sidebar: [
+            {
+                text: 'Introduction',
+                items: [
+                    { text: 'Test', link: '/test' },
+                ]
+            }
+        ]
+    }
+}
---
+++ docs/index.md
@@ -0,0 +1,15 @@
+---
+layout: home
+
+hero:
+  name: Unbound v1
+  text: Internal Documentation
+  tagline: Find your way around the interface
+  actions:
+    - theme: brand
+      text: Get Started
+      link: /test
+    - theme: alt
+      text: View Github for Interface
+      link: https://github.com/unbound-finance/unbound-interface
+---
